59e2dd509204708c232621e57454c9ee0b2100e6
Almaz Galimov <almazwork@gmail.com>
2025-02-20 18:54:42 +0500

    Add handlers

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"go/api-demo/internal/auth"
+	"net/http"
+)
+
+func main() {
+	// config := configs.LoadConfig()
+	router := http.NewServeMux()
+	auth.NewAuthHandler(router)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go/api-demo
+
+go 1.21.5
+
+require github.com/joho/godotenv v1.5.1
