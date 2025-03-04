90da30df11d45bbc585897fb4414c5af99ee0265
Almaz Galimov <almazwork@gmail.com>
2025-03-04 17:33:56 +0500

    Add link handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go/api-demo/configs"
 	"go/api-demo/internal/auth"
+	"go/api-demo/internal/auth/link"
 	"go/api-demo/pkg/db"
 	"net/http"
 )
@@ -12,9 +13,14 @@ func main() {
 	conf := configs.LoadConfig()
 	_ = db.NewDb(conf)
 	router := http.NewServeMux()
+
+	// Handlers
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config: conf,
 	})
+	link.NewLinkHandler(router, link.LinkHandlerDeps{
+	})
+
 
 	server := http.Server{
 		Addr:    ":8081",
