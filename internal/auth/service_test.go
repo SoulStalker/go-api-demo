4ce11099c705239b215ca6dc4ad0d88438ef2faf
Almaz Galimov <almazwork@gmail.com>
2025-03-27 18:19:47 +0500

    add mock test

---
+++ internal/auth/service_test.go
@@ -0,0 +1,32 @@
+package auth_test
+
+import (
+	"go/api-demo/internal/auth"
+	"go/api-demo/internal/user"
+	"testing"
+)
+
+type MockUserRepository struct {
+}
+
+func (repo *MockUserRepository) Create(u *user.User) (*user.User, error) {
+	return &user.User{
+		Email: "a@a.ru",
+	}, nil
+}
+
+func (repo *MockUserRepository) FindByEmail(email string) (*user.User, error) {
+	return nil, nil
+}
+
+func TestRegisterSuccess(t *testing.T) {
+	const initEmail = "a@a.ru"
+	authService := auth.NewAuthService(&MockUserRepository{})
+	email, err := authService.Register(initEmail, "1", "vasya")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if email != initEmail {
+		t.Fatalf("Email %s do not math %s", email, initEmail)
+	}
+}
--- pkg/di/interfaces.go
+++ pkg/di/interfaces.go
@@ -1,5 +1,13 @@
 package di
 
+import "go/api-demo/internal/user"
+
 type IStatRepository interface {
 	AddClick(linkId uint)
+}
+
+
+type IUserRepository interface {
+	Create(user *user.User) (*user.User, error)
+	FindByEmail(email string) (*user.User, error)
 }
